aadac2bb7c30ca750769156984f549f05944b531
1papaya <1love1papaya@gmail.com>
2021-02-26 15:33:55 +0000

    set rateLimitRetries to 10 on test-overpass

--- test/test-overpass.ts
+++ test/test-overpass.ts
@@ -6,6 +6,7 @@ import * as assert from "assert";
 describe("API Queries", function () {
   it("200 stream request", function () {
     return overpass(`[out:json]; node(626639517); out geom;`, {
+      rateLimitRetries: 10,
       verbose: true,
       stream: true,
     }).then((stream) => {
@@ -15,6 +16,7 @@ describe("API Queries", function () {
 
   it("200 JSON request", function () {
     return overpass(`[out:json]; node(626639517); out geom;`, {
+      rateLimitRetries: 10,
       verbose: true,
     }).then((json) => {
       json = json as OverpassJson;
@@ -26,6 +28,7 @@ describe("API Queries", function () {
 
   it("200 XML request", function () {
     return overpass(`[out:xml]; node(626639517); out geom;`, {
+      rateLimitRetries: 10,
       verbose: true,
     }).then((xml) => {
       xml = xml as string;
@@ -35,18 +38,21 @@ describe("API Queries", function () {
 
   it("400 bad request", function () {
     return overpass(`[out:json]; this aint gonna work`, {
+      rateLimitRetries: 10,
       verbose: true,
     }).catch((error) => console.log(error.message));
   });
 
   it("400 bad request", function () {
     return overpass(`[out:json]; this aint gonna work`, {
+      rateLimitRetries: 10,
       verbose: true,
     }).catch((error) => console.log(error.message));
   });
 
   it("bad url", function () {
     return overpass(`[out:json]; this aint gonna work`, {
+      rateLimitRetries: 10,
       verbose: true,
       endpoint: "//aint-gonna-work",
     }).catch((error) => console.log(error.message));
@@ -56,6 +62,7 @@ describe("API Queries", function () {
     for (let i = 0; i < 2; i++) overpass(`[out:json]; way[highway]; out geom;`);
 
     return overpass(`[out:json]; node(626639517); out geom;`, {
+      rateLimitRetries: 10,
       verbose: true,
     })
       .then((json) => {
