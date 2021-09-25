8f6ab1850a87ec17c1fa3f68345aad9f8e945555
Davey Lovin <me@geodavey.eu>
2021-09-25 13:29:43 +0200

    add public api endpoints

---
+++ src/endpoints.ts
@@ -0,0 +1,8 @@
+// public endpoints from https://wiki.openstreetmap.org/wiki/Overpass_API
+
+export const main = "https://overpass-api.de/api/interpreter";
+export const kumi = "https://overpass.kumi.systems/api/interpreter";
+export const france = "https://overpass.openstreetmap.fr/api/interpreter";
+export const switzerland = "https://overpass.osm.ch/api/interpreter";
+export const russia = "https://overpass.openstreetmap.ru/api/interpreter";
+export const usmil = "https://osm-overpass.gs.mil/overpass/interpreter"
