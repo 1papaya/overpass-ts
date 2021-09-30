ee02c1354f8836688967641c28baa45e1940f5cb
Davey Lovin <me@geodavey.eu>
2021-09-30 18:43:56 +0000

    update overpass tests for new api

--- test/test-overpass.ts
+++ test/test-overpass.ts
@@ -1,73 +1,74 @@
 import type { OverpassJson } from "../src/overpass-ts";
+import { kumi as kumiEndpoint } from "../src/endpoints";
 import * as nodeStream from "stream";
-import overpass from "../src/node";
+import {
+  overpassJson,
+  overpassCsv,
+  overpassXml,
+  overpassStream,
+} from "../src/overpass-ts";
 import * as assert from "assert";
 
 describe("API Queries", function () {
   it("200 stream request", function () {
-    return overpass(`[out:json]; node(626639517); out geom;`, {
-      endpoint: "//overpass.kumi.systems/api/interpreter",
-      rateLimitRetries: 10,
+    return overpassStream(`[out:json]; node(626639517); out geom;`, {
+      endpoint: kumiEndpoint,
       verbose: true,
-      stream: true,
     }).then((stream) => {
       assert.strictEqual(stream instanceof nodeStream.Readable, true);
     });
   });
 
   it("200 JSON request", function () {
-    return overpass(`[out:json]; node(626639517); out geom;`, {
-      endpoint: "//overpass.kumi.systems/api/interpreter",
-      rateLimitRetries: 10,
+    return overpassJson(`[out:json]; node(626639517); out geom;`, {
+      endpoint: kumiEndpoint,
       verbose: true,
     }).then((json) => {
-      json = json as OverpassJson;
-      assert.strictEqual("elements" in json, true);
+      assert.doesNotThrow(function () {
+        json as OverpassJson;
+      });
 
       console.log(json);
     });
   });
 
   it("200 XML request", function () {
-    return overpass(`[out:xml]; node(626639517); out geom;`, {
-      endpoint: "//overpass.kumi.systems/api/interpreter",
-      rateLimitRetries: 10,
+    return overpassXml(`[out:xml]; node(626639517); out geom;`, {
+      endpoint: kumiEndpoint,
       verbose: true,
     }).then((xml) => {
-      xml = xml as string;
+      assert.doesNotThrow(function () {
+        xml as string;
+      });
+
+      console.log(xml);
+    });
+  });
+
+  it("200 CSV request", function () {
+    return overpassCsv(`[out:csv(::type)]; node(626639517); out geom;`, {
+      endpoint: kumiEndpoint,
+      verbose: true,
+    }).then((xml) => {
+      assert.doesNotThrow(function () {
+        xml as string;
+      });
+
       console.log(xml);
     });
   });
 
   it("400 bad request", function () {
-    return overpass(`[out:json]; this aint gonna work`, {
-      endpoint: "//overpass.kumi.systems/api/interpreter",
-      rateLimitRetries: 10,
+    return overpassJson(`[out:json]; this aint gonna work`, {
+      endpoint: kumiEndpoint,
       verbose: true,
     }).catch((error) => console.log(error.message));
   });
 
   it("bad url", function () {
-    return overpass(`[out:json]; this aint gonna work`, {
-      rateLimitRetries: 10,
+    return overpassJson(`[out:json]; this aint gonna work`, {
       verbose: true,
       endpoint: "//aint-gonna-work",
     }).catch((error) => console.log(error.message));
-  });
-
-  it("409 too many requests", function () {
-    for (let i = 0; i < 2; i++) overpass(`[out:json]; way[highway]; out geom;`);
-
-    return overpass(`[out:json]; node(626639517); out geom;`, {
-      endpoint: "//overpass.kumi.systems/api/interpreter",
-      rateLimitRetries: 10,
-      verbose: true,
-    })
-      .then((json) => {
-        console.log(json);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
   });
 });
