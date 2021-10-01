1a9173775ae2aad2b38e50c7f85587141f68d26c
Davey Lovin <me@geodavey.eu>
2021-10-01 09:50:29 +0000

    add live status data test

--- test/test-status.ts
+++ test/test-status.ts
@@ -4,6 +4,7 @@ import { parseApiStatus } from "../src/utils";
 import { apiStatus, OverpassApiStatusError } from "../src/overpass-ts";
 import { OverpassApiStatus } from "../src/types";
 import {
+  kumi as kumiEndpoint,
   switzerland as switzerlandEndpoint,
   france as franceEndpoint,
 } from "../src/endpoints";
@@ -20,13 +21,18 @@ describe("Overpass API Status", function () {
     );
   });
 
+  it("Works correctly on live data", function () {
+    return apiStatus(kumiEndpoint).then((apiStatus) => {
+      console.log(apiStatus);
+    });
+  });
+
   it("Broken switzerland /status endpoint errors correctly", function () {
     return apiStatus(switzerlandEndpoint).catch((error) => {
       assert(error instanceof OverpassApiStatusError);
       console.log(error.message);
     });
   });
-
 
   it("Broken france /status endpoint errors correctly", function () {
     return apiStatus(franceEndpoint).catch((error) => {
