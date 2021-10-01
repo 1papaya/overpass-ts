3e0588306049fdfd8c23582555c4bcdbc30729fd
Davey Lovin <me@geodavey.eu>
2021-10-01 10:17:44 +0000

    remove replaceAll &quot; on 400 error

--- src/overpass.ts
+++ src/overpass.ts
@@ -47,9 +47,10 @@ export function overpass(
         // if bad request, error details sent along as html
         // load the html and parse it for detailed error
 
-        const errors = utils
-          .matchAll(/<\/strong>: ([^<]+) <\/p>/g, await resp.text())
-          .map((error) => error.replaceAll("&quot;", '"'));
+        const errors = utils.matchAll(
+          /<\/strong>: ([^<]+) <\/p>/g,
+          await resp.text()
+        );
 
         throw new OverpassBadRequestError(query, errors);
       } else if (resp.status === 429) {
